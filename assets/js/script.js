6b8a9aa7872c744123c717344ac4580967c8a11e
maibelline quilliet <mayb80@hotmail.fr>
2022-01-18 12:05:51 +0100

    partie 1

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+let message= 'Bonjour';
+alert(message);
+
+let firstPart= 'salut,';
+let lastPart= ' comment ca va?';
+alert(firstPart+''+lastPart);
+
+let firstName= 'Maibelline';
+let age= 38;
+let city='St sauflieu';
+alert('Bonjour, je m\'appelle'+' '+firstName+',j\'ai '+' '+age+',et j\'habite à '+' '+city+'');
+
+let myName= prompt ('Qu\'est votre nom ?','Entrez votre nom');
+let myAge= prompt ('Quel est votre age ?',' Entrez votre age');
+let myCity= prompt ('Quel est votre ville ?','Entrez votre ville');
+alert('Bonjour, tu t\'appelles '+myName+' tu as '+myAge+' et tu vis à'+myCity);
+
+let firstNumber= prompt ('Le premier nombre','Entrez le premier nombre');
+let secondNumber= prompt ('Le second chiffre','Entrez le second nombre');
+resulCalc = parseInt(firstNumber, 10) + parseInt(secondNumber, 10);
+alert('La somme de '+firstNumber+' et '+secondNumber+' est égale à: '+resulCalc );
+
+
